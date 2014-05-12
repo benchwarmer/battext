4b9ae6ab3cd052bdc47ef4c5cfd3a65cf08c98e2
benchwarmer <dschevyvette@gmail.com>
2014-05-12 01:01:21 -0500

    The contact field picker now changes depending on what type of alert you select.

--- src/com/drew/BatText/BatTextActivity.java
+++ src/com/drew/BatText/BatTextActivity.java
@@ -45,14 +45,26 @@ public class BatTextActivity extends Activity {
     public String prefAlertTypePrefix = "alertCard_alertType_";
     public String prefContactNamePrefix = "alertCard_contact_name_";
     public String prefContactNumberPrefix = "alertCard_contact_number_";
+    public String prefContactEmailPrefix = "alertCard_contact_email_";
     
     private static final int PICK_CONTACT = 1234;
+    private static final int PICK_EMAIL = 1235;
+    
+    private String[] alertTypeOptions;
+    private String alertTypeOptions_text;
+    private String alertTypeOptions_email;
+    private String alertTypeOptions_other;
     
 	
     
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
+		alertTypeOptions = context.getResources().getStringArray(R.array.alerttype_options);
+		alertTypeOptions_text = alertTypeOptions[0];
+		alertTypeOptions_email = alertTypeOptions[1];
+		alertTypeOptions_other = alertTypeOptions[2];
+		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.list_alertcard_undo);
 
@@ -128,12 +140,23 @@ public class BatTextActivity extends Activity {
 	        if (c.moveToFirst()) {
 	          String name = c.getString(c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
 	          String number = c.getString(c.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-	          Log.d("onActivityResultContact", "Contact Number: " + number);
-	          setContact(name, number, mCurrentCardIdStr); 
+	          setContact(name, number, "", mCurrentCardIdStr, reqCode); 
 	          
 	        }
 	      }
 	      break;
+	    case (PICK_EMAIL) :
+		      if (resultCode == Activity.RESULT_OK) {
+		        Uri contactData = data.getData();
+		        Cursor c =  getContentResolver().query(contactData, null, null, null, null);
+		        if (c.moveToFirst()) {
+		          String name = c.getString(c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+		          String email = c.getString(c.getColumnIndex(ContactsContract.CommonDataKinds.Email.ADDRESS));
+		          setContact(name, "", email, mCurrentCardIdStr, reqCode); 
+		          
+		        }
+		      }
+		      break; 
 	  }
 	}
 	
@@ -148,10 +171,10 @@ public class BatTextActivity extends Activity {
 		return PreferenceManager.getDefaultSharedPreferences(context).getString(prefAlertTypePrefix + alertId, "");
 	}
 	
-	public void setContact(String name, String number, String alertId) {
+	public void setContact(String name, String number, String email, String alertId, int reqType) {
 		int positionFromId =  java.lang.Integer.parseInt(alertId);  
 		AlertCard card = (AlertCard) mCardArrayAdapter.getItem(positionFromId);
-		card.setContact(name, number);
+		card.setContact(name, number, email, reqType);
 	}
 	
 	public String getContact(String alertId) {
@@ -168,7 +191,8 @@ public class BatTextActivity extends Activity {
 		protected TextView mContact;
 		protected TextView mBatteryPercent;
 		
-		
+		private String alertTypeStr;
+		private String contactStr;
 
         /**
          * Constructor with a custom inner layout
@@ -212,8 +236,8 @@ public class BatTextActivity extends Activity {
         	// Setup alertType
         	mAlertType = (TextView) parent.findViewById(R.id.main_inner_alerttype);
         	if(mAlertType!=null) {
-        		String alertTypeStr = PreferenceManager.getDefaultSharedPreferences(context).getString(prefAlertTypePrefix + thisCard.getId(), "");
-        		if (alertTypeStr != "") {
+        		alertTypeStr = PreferenceManager.getDefaultSharedPreferences(context).getString(prefAlertTypePrefix + thisCard.getId(), "");
+        		if (!alertTypeStr.equals("")) {
         			mAlertType.setText(alertTypeStr);
         		}
         		mAlertType.setOnClickListener(new View.OnClickListener() {
@@ -232,26 +256,25 @@ public class BatTextActivity extends Activity {
         	// Setup contact
         	mContact = (TextView) parent.findViewById(R.id.main_inner_contact);
         	if(mContact!=null) {
-        		String contactStr = PreferenceManager.getDefaultSharedPreferences(context).getString(prefContactNamePrefix + thisCard.getId(), "");
-        		if (contactStr != "") {
+        		contactStr = PreferenceManager.getDefaultSharedPreferences(context).getString(prefContactNamePrefix + thisCard.getId(), "");
+        		if (!contactStr.equals("")) {
         			mContact.setText(contactStr);
         		}
         		mContact.setOnClickListener(new View.OnClickListener() {
       		      @Override
       		      public void onClick(View v) {
       		    	mCurrentCardIdStr = thisCard.getId();
-      		    	
       		    	Intent intent = new Intent(Intent.ACTION_PICK);
-      		    	intent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
-      		    	startActivityForResult(intent, PICK_CONTACT);
-      		    	
-					
+      		    	if(alertTypeStr.equals(alertTypeOptions_text)) {
+	      		    	intent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
+	      		    	startActivityForResult(intent, PICK_CONTACT);
+      		    	} else if (alertTypeStr.equals(alertTypeOptions_email)) {
+      		    		intent.setType(ContactsContract.CommonDataKinds.Email.CONTENT_TYPE);
+	      		    	startActivityForResult(intent, PICK_EMAIL);
+      		    	}
       		      }
-      	
       		    });
-        	} else {
-        		Log.d("AlertCard", "AlertType TextView was not found.");
-        	}
+        	} 
 
         }
         
@@ -262,22 +285,26 @@ public class BatTextActivity extends Activity {
     		SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(context).edit();
     		
         	if(!alertTypeView.getText().equals(alertType)) {
+        		alertTypeStr = alertType;
         		alertTypeView.setText(alertType);
             	contactView.setText(R.string.inner_special_text_1);
             	editor.putString(prefAlertTypePrefix + thisCard.getId(), alertType);
             	editor.putString(prefContactNumberPrefix + thisCard.getId(), getString(R.string.inner_special_text_1));
         		editor.putString(prefContactNamePrefix + thisCard.getId(), getString(R.string.inner_special_text_1));
         	}
-        	
         	editor.commit();
         }
         
-        public void setContact(String name, String number) {
+        public void setContact(String name, String number, String email, int reqType) {
         	TextView contactView = (TextView) mCardView.findViewById(R.id.main_inner_contact);
         	contactView.setText(name);
         	SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(context).edit();
-        	editor.putString(prefContactNumberPrefix + thisCard.getId(), number);
-    		editor.putString(prefContactNamePrefix + thisCard.getId(), name);
+        	editor.putString(prefContactNamePrefix + thisCard.getId(), name);
+        	if(reqType == PICK_CONTACT) {
+        		editor.putString(prefContactNumberPrefix + thisCard.getId(), number);
+        	} else if (reqType == PICK_EMAIL) {
+        		editor.putString(prefContactEmailPrefix + thisCard.getId(), email);
+        	}
     		editor.commit();
         	
         }
